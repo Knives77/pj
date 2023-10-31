0d26499db9a565bdc4cc92d3af6c4d02de705091
Knives77 <Knives77@github.com>
2023-10-31 17:06:57 -0600

    Actualización de tablas v_knives 2

--- sql/tables.sql
+++ sql/tables.sql
@@ -18,9 +18,12 @@ CREATE TABLE albums (
   name_a VARCHAR(30) NOT NULL DEFAULT "",
   alb_cov VARCHAR(200) NOT NULL,
   date_alb DATE NOT NULL,
+  totl_trck INT NOT NULL,
+  gen_alb VARCHAR (100) NOT NULL,
   punct DECIMAL(3,1) CHECK (punct >= 0 AND punct <= 10),
   test VARCHAR(150) NOT NULL,
   price DECIMAL (5,2) CHECK (price >= 0 AND price <= 1000),
+  note VARCHAR (150) NOT NULL,
   PRIMARY KEY (id_alb)
 );
 DESC albums;
@@ -28,11 +31,13 @@ DESC albums;
 --Creamos la tabla  "songs" 
 CREATE TABLE songs (
   id_song INT AUTO_INCREMENT,
+  id_alb INT,
   name_s VARCHAR (60) NOT NULL,
   date_s DATE NOT NULL,
   durtn TIME NOT NULL,
   rptn BIGINT NOT NULL,
-  id_alb INT,
+  price_s DECIMAL (3,2) CHECK (price_s >= 0 AND price_s <= 5),
+  letra VARCHAR (100) NOT NULL,
   FOREIGN KEY(id_alb) REFERENCES albums(id_alb) ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY(id_song)
 );
@@ -46,9 +51,14 @@ CREATE TABLE users (
   ap_m_usr VARCHAR(60) NOT NULL,
   age INT(3) NOT NULL CHECK (age >= 0 AND age <= 130),
   gen VARCHAR(6) NOT NULL CHECK (gen = 'Hombre' OR gen = 'Mujer'),
-  num VARCHAR(15) NOT NULL,
+  street VARCHAR (50) NOT NULL,
+  num_house INT(4) NOT NULL ,
+  code_post int (5) NOT NULL CHECK (code_post >= 0 AND code_post <= 99999),
+  colony VARCHAR (50) NOT NULL,
+  munic VARCHAR (50) NOT NULL,
+  state VARCHAR (50) NOT NULL,
+  phone_n VARCHAR(15) NOT NULL,
   email VARCHAR(100) NOT NULL,
-  nickname VARCHAR(70) NOT NULL,
   psswd VARCHAR(70) NOT NULL,
   PRIMARY KEY(id_usr)
 );
@@ -56,7 +66,11 @@ DESC users;
 
 CREATE TABLE sales (
   id_sale INT AUTO_INCREMENT,
-  PRIMARY KEY(id_sale)
+  date_sale DATE NOT NULL,
+  id_usr INT,
+  ttl_sale DECIMAL(6,2) CHECK (ttl_sale >= 0 AND ttl_sale <= 5500),
+  PRIMARY KEY(id_sale),
+  FOREIGN KEY(id_usr) REFERENCES users(id_usr) ON UPDATE CASCADE ON DELETE CASCADE
 );
 DESC sales;
 
@@ -64,14 +78,18 @@ DESC sales;
 --Insert de "albums" 
 INSERT INTO 
   albums
-VALUES (NULL, "YIELD","https://drive.google.com/file/d/1Pm9GhHB4FzGFqNuPEDHPtiTYPAreXfUH/view?usp=drivesdk","2023-12-15", 0.1, "", 9.99);
+VALUES (NULL, "YIELD","../img/Yield.jpg","2023-12-15", 12, "grunge",0.1, "", 9.99, "Mi introducción");
 
 --Insert de "songs" 
 INSERT INTO 
   songs
-VALUES (NULL, "MFC", "2023-10-31", "00:02:26", 4555, 1);
+VALUES (NULL, 1, "MFC", "2023-10-31", "00:02:26", 4555, 5, "***");
 
 --Insert de "users" 
 INSERT INTO 
   users 
-VALUES (NULL, "Knives", "77", "_", 130, "Mujer", "123-456-7890","a@a.com", "Re-L Mayer", "***");
+VALUES (NULL, "Knives", "77", "_", 130, "Mujer", "street", 
+        9999, 76700, "colony", "municipaly", "state","123-456-7890", 
+       "a@a.com", "***");
+
+
