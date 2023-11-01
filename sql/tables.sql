65ace4ec4f1f2e3c8db2bbe3e42dc24f80c49159
Knives77 <Knives77@github.com>
2023-10-31 20:28:14 -0600

    Actualización de tabla (inset)

--- sql/tables.sql
+++ sql/tables.sql
@@ -4,18 +4,21 @@
    v: Knives
 */
 
---Verificamos si existe una base de datos con 
---el mismo nombre y la eliminamos. Posteriormente
---creamos la base de datos y selecionamos.
+/*
+Verificamos si existe una base de datos con 
+el mismo nombre y la eliminamos. Posteriormente
+creamos la base de datos y selecionamos.
+*/
 
 DROP DATABASE IF EXISTS pj;
 CREATE DATABASE pj CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SET NAMES "utf8mb4";
 USE pj;
 
---Creamos la tabla "album"
+/*Creamos la tabla "album"*/
 CREATE TABLE albums (
   id_alb INT AUTO_INCREMENT,
-  name_a VARCHAR(30) NOT NULL DEFAULT "",
+  name_a VARCHAR(30) NOT NULL,
   alb_cov VARCHAR(200) NOT NULL,
   date_alb DATE NOT NULL,
   totl_trck INT NOT NULL,
@@ -23,12 +26,13 @@ CREATE TABLE albums (
   punct DECIMAL(3,1) CHECK (punct >= 0 AND punct <= 10),
   test VARCHAR(150) NOT NULL,
   price DECIMAL (5,2) CHECK (price >= 0 AND price <= 1000),
-  note VARCHAR (150) NOT NULL,
+  note VARCHAR(1000) NOT NULL,
   PRIMARY KEY (id_alb)
 );
+
 DESC albums;
 
---Creamos la tabla  "songs" 
+/*Creamos la tabla  "songs" */
 CREATE TABLE songs (
   id_song INT AUTO_INCREMENT,
   id_alb INT,
@@ -37,13 +41,13 @@ CREATE TABLE songs (
   durtn TIME NOT NULL,
   rptn BIGINT NOT NULL,
   price_s DECIMAL (3,2) CHECK (price_s >= 0 AND price_s <= 5),
-  letra VARCHAR (100) NOT NULL,
+  letra VARCHAR (200) NOT NULL,
   FOREIGN KEY(id_alb) REFERENCES albums(id_alb) ON UPDATE CASCADE ON DELETE CASCADE,
   PRIMARY KEY(id_song)
 );
 DESC songs;
 
---Creamos la tabla "users" 
+/*Creamos la tabla "users" */
 CREATE TABLE users (
   id_usr INT NOT NULL AUTO_INCREMENT,
   name_usr VARCHAR(60) NOT NULL,
@@ -64,7 +68,7 @@ CREATE TABLE users (
 );
 DESC users;
 
---Creamos la tabla "sales" 
+/*Creamos la tabla "sales" */
 CREATE TABLE sales (
   id_sale INT AUTO_INCREMENT,
   id_usr INT,
@@ -76,7 +80,7 @@ CREATE TABLE sales (
 );
 DESC sales;
 
---Creamos la tabla "album_sales" 
+/*Creamos la tabla "album_sales" */
 CREATE TABLE album_sales (
   id_sale INT,
   id_alb INT,
@@ -86,7 +90,7 @@ CREATE TABLE album_sales (
 );
 DESC album_sales;
 
---Creamos la tabla "song_sales" 
+/*Creamos la tabla "song_sales" */
 CREATE TABLE song_sales (
   id_sale INT,
   id_song INT,
@@ -96,7 +100,7 @@ CREATE TABLE song_sales (
 );
 DESC song_sales;
 
---Creamos la tabla "combined_sales" 
+/*Creamos la tabla "combined_sales" */
 CREATE TABLE combined_sales (
   id_sale INT,
   id_alb INT,
@@ -108,21 +112,44 @@ CREATE TABLE combined_sales (
 );
 DESC combined_sales;
 
---Insert de "albums" 
+/*Insert de "albums" */
 INSERT INTO 
   albums
-VALUES (NULL, "YIELD","../img/Yield.jpg","2023-12-15", 12, "grunge",0.1, "", 9.99, "Mi introducción");
+VALUES 
+(NULL, 'YIELD',"../img/Yield.jpg","1998-02-03", 
+12, "Grunge, Rock Alternativo", 9.5, "_", 9.99, 
+"El quinto álbum de estudio de Pearl Jam, 'Yield', continúa la
+evolución musical de la banda, fusionando elementos grunge con 
+una mayor diversidad de estilos. Destaca por su enfoque lírico 
+y musical maduro."),
+(NULL, "Ten", "ruta", "1991-08-27", 11, "Grunge, Rock Alternativo", 10.0,
+"_", 9.99, "Uno de los álbumes más influyentes de los años 90.");
 
---Insert de "songs" 
+
+/*Insert de "songs" */
 INSERT INTO 
   songs
 VALUES (NULL, 1, "MFC", "2023-10-31", "00:02:26", 4555, 5, "***");
 
---Insert de "users" 
+/*Insert de "users" */
 INSERT INTO 
   users 
 VALUES (NULL, "Knives", "77", "_", 130, "Mujer", "street", 
         9999, 76700, "colony", "municipaly", "state","123-456-7890", 
        "a@a.com", "***");
 
+/*Insert de "sales" */
+INSERT INTO sales 
+ VALUES (NULL, 1, "2023-10-31", 5500.00, "Tarjeta de credito");
 
+/*Insert de "album_sales" */
+INSERT INTO album_sales 
+  VALUES (1, 1);
+
+/*Insert de "song_sales" */
+INSERT INTO song_sales 
+  VALUES (1, 1);
+
+/*Insert de "combined_sales" */
+INSERT INTO combined_sales
+ VALUES (1,1,1)
